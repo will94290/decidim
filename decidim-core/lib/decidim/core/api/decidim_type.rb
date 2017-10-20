47fc0ed468d1d441e27971fe38cdfda82a3b4f4d
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2017-10-20 15:41:41 +0200

    Fix version field in decidim type

--- decidim-core/lib/decidim/core/api/decidim_type.rb
+++ decidim-core/lib/decidim/core/api/decidim_type.rb
@@ -7,7 +7,7 @@ module Decidim
     description "Decidim's framework-related properties."
 
     field :version, !types.String, "The current decidim's version of this deployment." do
-      resolve ->(obj, _args, _ctx) { obj.version }
+      resolve ->(_obj, _args, _ctx) { Gem.loaded_specs["decidim"].version }
     end
 
     field :application_name, !types.String, "The current installation's name." do
