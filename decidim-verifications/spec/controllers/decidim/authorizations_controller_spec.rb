98a9f663c2c0c3daede24eaf541314dfc940a397
David Rodríguez <deivid.rodriguez@riseup.net>
2017-10-20 14:46:51 +0200

    Move `AuthorizationsController` to decidim-verifications
    Add a new `Decidim::Verifications::Engine` that mounts all available
    authorization workflows.

--- decidim-verifications/spec/controllers/decidim/authorizations_controller_spec.rb
+++ decidim-verifications/spec/controllers/decidim/authorizations_controller_spec.rb
@@ -2,9 +2,9 @@
 
 require "spec_helper"
 
-module Decidim
+module Decidim::Verifications
   describe AuthorizationsController, type: :controller do
-    routes { Decidim::Core::Engine.routes }
+    routes { Decidim::Verifications::Engine.routes }
 
     include_context "authenticated user"
 
